3b46968d77dc69fa098f55da7d3013105edf7c19
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-25 15:52:46 -0700

    connect to postgresql db

--- wearitout/asgi.py
+++ wearitout/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mycloset project.
+ASGI config for wearitout project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycloset.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wearitout.settings')
 
 application = get_asgi_application()
--- wearitout/wsgi.py
+++ wearitout/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mycloset project.
+WSGI config for wearitout project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycloset.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wearitout.settings')
 
 application = get_wsgi_application()
