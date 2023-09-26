c385528e093ed9ed0c422939bfa658667286531c
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-26 13:04:07 -0700

    add tag property on clothing item

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import ClothingItem, Color
+from .models import ClothingItem, Color, Tag
 
 # Register your models here.
 admin.site.register(ClothingItem)
 admin.site.register(Color)
+admin.site.register(Tag)
--- main_app/models.py
+++ main_app/models.py
@@ -15,6 +15,12 @@ class Color(models.Model):
 
     def __str__(self):
         return self.color
+    
+class Tag(models.Model):
+    tag=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tag
 
 class ClothingItem(models.Model):
     description= models.CharField(max_length=100)
@@ -24,6 +30,7 @@ class ClothingItem(models.Model):
     place_purchased= models.CharField(max_length=100)
     price= models.DecimalField(decimal_places=2, max_digits=10)
     size= models.CharField(max_length=4)
+    tags=models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.description
