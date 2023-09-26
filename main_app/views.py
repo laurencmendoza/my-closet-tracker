999be493c5a4e97f2e30efbb7c9960ca063fc0af
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-25 22:56:06 -0700

    create urls and views for each navbar element

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,15 @@ from django.shortcuts import render
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+def clothing_items_index(request):
+  return render(request, 'closet.html')
+
+def add_clothing_item(request):
+  return render(request, 'add_clothing_item.html')
+
+def outfits_index(request):
+  return render(request, 'outfits.html')
+
+def outfit_tracker(request):
+  return render(request, 'outfit_tracker.html')
