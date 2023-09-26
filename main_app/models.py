e97c7da71ec810556fb7b7d169df05cee527a405
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-26 11:57:11 -0700

    add color property to clothing items

--- main_app/models.py
+++ main_app/models.py
@@ -10,9 +10,16 @@ CATEGORIES = [
     ("S", "Shoes"),
 ]
 
+class Color(models.Model):
+    color=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.color
+
 class ClothingItem(models.Model):
     description= models.CharField(max_length=100)
     category= models.CharField(max_length=1, choices=CATEGORIES, default=CATEGORIES[0][0])
+    colors=models.ManyToManyField(Color)
     date_acquired= models.DateField(auto_now=True)
     place_purchased= models.CharField(max_length=100)
     price= models.DecimalField(decimal_places=2, max_digits=10)
