70329458ffcb1a0607454cade7a07c463dd8f567
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-26 20:40:59 -0700

    add login functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORIES = [
@@ -31,6 +32,7 @@ class ClothingItem(models.Model):
     price= models.DecimalField(decimal_places=2, max_digits=10)
     size= models.CharField(max_length=4)
     tags=models.ManyToManyField(Tag)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description
